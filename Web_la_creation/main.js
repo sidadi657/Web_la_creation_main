cd2b62055ad9a2fd144aa6a769ccf38ecdb8ca87
sidadi657 <sidadi1804@gmail.com>
2023-07-27 01:50:42 +0530

    A hugeeeeeeeeeeeeeee commit
    cart,js,images,headache and a lot of things

--- Web_la_creation/main.js
+++ Web_la_creation/main.js
@@ -9,7 +9,7 @@ let products = [{
 },
 ]
 
-
+let productNumbers=0;
 for (let i = 0; i < carts.length; i++) {
     carts[i].addEventListener('click', () => {
         let exist = 0;
@@ -28,23 +28,40 @@ for (let i = 0; i < carts.length; i++) {
             products[i] = {
                 name: event.target.parentElement.children[0].textContent,
                 tag: event.target.parentElement.children[0].textContent.toLowerCase().replace(/\s/g, ""),
-                price: event.target.parentElement.children[1].textContent,
+                price: event.target.parentElement.children[3].textContent.replace('₹', ''),
                 incart: 0
             }
         }
 
         cartNumbers(products[i]);
+        totalCost(products[i]);
+        
     })
 }
 function onLoadCartNo() {
     let productNumbers = localStorage.getItem('cartNumbers');
+    let cartCost=localStorage.getItem("totalCost");
+    if(cartCost)
+    {
+        if ( document.URL.includes("cart.html") ) {
+            document.querySelector('.total h2').textContent = "₹"+parseInt(cartCost);
+        }
+        document.querySelector('.cost span').textContent = "₹"+parseInt(cartCost);
+    }
     if (productNumbers) {
         document.querySelector('.item-no span').textContent = productNumbers;
+    }
+    if (document.querySelector('.item-no span').textContent >= 1) {
+        document.getElementById("cart").style.visibility = "visible";
+    }
+    else {
+        document.getElementById("cart").style.visibility = "hidden";
     }
 }
 function cartNumbers(product) {
 
-    let productNumbers = localStorage.getItem('cartNumbers');
+    productNumbers = localStorage.getItem('cartNumbers');
+    
     productNumbers = parseInt(productNumbers);
     if (productNumbers) {
         localStorage.setItem('cartNumbers', productNumbers + 1);
@@ -54,18 +71,20 @@ function cartNumbers(product) {
         document.querySelector('.item-no span').textContent = 1;
     }
     setItems(product);
-    if (productNumbers > 0) {
+    if (document.querySelector('.item-no span').textContent >= 1) {
         document.getElementById("cart").style.visibility = "visible";
     }
     else {
         document.getElementById("cart").style.visibility = "hidden";
     }
+ 
+    
+    
 }
 
 function setItems(product) {
     let cartItems = localStorage.getItem('productsInCart');
     cartItems = JSON.parse(cartItems);
-    console.log("My cartItems are", cartItems);
     if (cartItems != null) {
 
         if(cartItems [product.tag] == undefined) {
@@ -88,5 +107,56 @@ function setItems(product) {
 
     localStorage.setItem("productsInCart", JSON.stringify(cartItems));
 }
+function totalCost(product)
+{
+    product.price=parseInt(product.price);
+    let cartCost=localStorage.getItem("totalCost");
+    if(cartCost!=null)
+    {
+        cartCost=parseInt(cartCost)+product.price;
+        localStorage.setItem("totalCost", parseInt(cartCost));
+    }
+    else
+    {
+        cartCost=product.price;
+        localStorage.setItem("totalCost", parseInt(cartCost));
+    }
+    document.querySelector('.cost span').textContent = "₹"+parseInt(cartCost);
+    console.log(cartCost);
+    console.log(typeof product.price);
+    document.querySelector('.total h2').textContent = "₹"+parseInt(cartCost);
 
+}
+
+function displayCart()
+{
+    let cartItems = localStorage.getItem("productsInCart");
+    cartItems=JSON.parse(cartItems);
+    let productContainer=document.querySelector(".products");
+    if(cartItems && productContainer)
+    {
+        productContainer.innerHTML='';
+        Object.values(cartItems).map(item =>{
+            productContainer.innerHTML +=`
+            <div class="row product-row">
+            <i class="fa-solid fa-circle-xmark col-1"></i>
+            <div class="product col-3">
+                
+                <img src="./images/${item.tag}.jpg">
+                <span>${item.name}</span>
+            </div>
+            <div class="price col-3">₹${item.price}</div>
+            <div class="quantity col-2">
+            
+            ${item.incart}
+            
+            </div>
+            <div class="price col-3">₹${item.incart*item.price}</div>
+            </div>
+            `
+        });
+        
+    }
+}
+displayCart();
 onLoadCartNo();
